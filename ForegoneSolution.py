eba70214e88eb5b96534c4dae354575138c65f25
avirias <avirias@live.com>
2019-04-17 04:16:47 +0530

    Initial from PyCharm

--- ForegoneSolution.py
+++ ForegoneSolution.py
@@ -1,18 +1,19 @@
 import random
+
 times = int(input())
 cases = []
 
+
 def cal(case):
-    while(1):
-        no = random.randint(1,case)
-        if('4' not in list(str(no))):
-            if('4' not in list(str(case-no))):
+    while True:
+        no = random.randint(1, case)
+        if '4' not in list(str(no)):
+            if '4' not in list(str(case - no)):
                 return no
-    
-    
-    
+
+
 for i in range(times):
     cases.append(int(input()))
 for i in range(times):
     no = cal(cases[i])
-    print("Case #"+str(i+1)+": "+str(no)+" "+str(cases[i]-no))
+    print("Case #" + str(i + 1) + ": " + str(no) + " " + str(cases[i] - no))
